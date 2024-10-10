c651b0ed042c8fe80385b006233cecef24648c89
savki <savkin@altacod.ru>
2024-10-10 22:28:02 +0300

    initial commit

---
+++ src/main/java/com/example/diagramapp/DiagramappApplication.java
@@ -0,0 +1,13 @@
+package com.example.diagramapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagramappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiagramappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diagramapp/DiagramappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diagramapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagramappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
